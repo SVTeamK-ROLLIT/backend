b179c26c0c7cd3805867d61cb974188cb210f556
Jung Yunho <a01049048063@gmail.com>
2023-01-06 17:46:29 +0900

    feat: 회원가입 주요 로직 구현

--- backend/rollpaper/urls.py
+++ backend/rollpaper/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'rollpaper'
 
 urlpatterns = [
-    path('users/signup', views.signup),
+    path('users/signup', views.sign_up),
 ]
--- backend/rollpaper/views.py
+++ backend/rollpaper/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rollpaper.models import User
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+
+
+@api_view(['POST'])
+def sign_up(request):
+    email = request.data['email']
+    password = request.data['password']
+    nickname = request.data['nickname']
+    result = User.objects.create(email = email, password = password, nickname = nickname)
+    user_id = result.id 
+    return JsonResponse({"user_id": user_id}, status=200)
