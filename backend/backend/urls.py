7249dc53fc33b79bc0f19d15a79e9bd8fd4f1baa
Yejin <say0049@naver.com>
2023-01-17 17:25:12 +0900

    fix : prometheus.yml, docker-compose.yaml 등 잘못 설정한 파일들 수정
    프로메테우스 파일과 장고 메트릭 설정을 잘못해서 그라파나에서 요청이 뜨지 않던 오류 수정

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,5 +6,7 @@ from rollpaper import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include('rollpaper.urls'))
+    path('api/v1/',include('rollpaper.urls')),
+    #promethus
+    path("", include("django_prometheus.urls"))
 ]
