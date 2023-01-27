65dd4c7c792e2dbbd3963d3a76b142187a734e37
Sangwon <qmfdnjssk@tukorea.ac.kr>
2023-01-27 22:25:31 +0900

    feat:memo삭제 url수정

--- backend/rollpaper/urls.py
+++ backend/rollpaper/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('users/signup',views.sign_up), #회원가입
     path('papers/<int:paper_id>/photos',views.photo),#사진 추가
     path('papers/<int:paper_id>/memos',views.memo), #메모 생성
-    path('papers/<int:memo_id>',views.memo_delete), #메모 삭제
+    path('papers/memos/<int:memo_id>',views.memo_delete), #메모 삭제
     path('papers/<int:paper_id>/stickers',views.stickers), #스티커를 추가
     path('users/<int:user_id>', views.my_page, name="my_page"), #user_id라는 것이 존재x, User 테이블의 id를 입력받음
     path('papers/<int:paper_id>/',views.get_paper), #paper의 memo,image,sticker의 정보를 가져옵니다
